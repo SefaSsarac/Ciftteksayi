733e8a4faa0595d41d38318320212c5938cb0c1a
Sefa Saraç <srcsfysf@gmail.com>
2024-07-17 18:28:32 +0300

    Add files via upload

---
+++ If-else/If-else/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Lutfen bir sayi giriniz: ");
+
+        string girilensayi = Console.ReadLine();
+
+        int sayi = Convert.ToInt32(girilensayi);    // Kullanıcının girdigi degeri integer a donusturuyorum.
+
+        if (sayi == 10)                        // Sayının 10'a eşit olup olmadığını kontrol ediyorum
+        {
+            Console.WriteLine("Girdiğiniz sayı 10'a eşittir.");
+        }
+        else if (sayi < 10)                   // Sayının 10'dan küçük olup olmadığını kontrol ediyorum
+        {
+            Console.WriteLine("Girdiğiniz sayı 10'dan küçüktür. ");
+        }
+        else                                  // Sayının 10'dan büyük olup olmadığını kontrol ediyorum
+        {
+            Console.WriteLine("Girdiğiniz sayı 10'dan büyüktür.");
+        }
+        if (sayi % 2 == 0)                   // Sayının cift sayı olup olmadiginı kontrol ediyorum
+        {
+            Console.WriteLine("Girilen sayi cifttir. ");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayi tektir. ");
+        }
+    }
+}
+
+
+
+
+
